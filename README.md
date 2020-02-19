aa2ab6400d8f83715c8d9528a78dbe23e1e37e02
s-yoshida <yos.3104@gmail.com>
2020-02-20 02:28:07 +0900

    テスト（jest）関連追加

--- README.md
+++ README.md
@@ -39,7 +39,29 @@ const hello2 = name => `Hello ${namae}!`
 VSCode 上で赤波線が 5 箇所出ているでしょうか？問題なく表示されていれば ESLint は正常に動いています！  
 続いて保存してみましょう。4 行目のセミコロンが消えて赤波線が 4 箇所になりましたか？そうなっていれば Prettir も正常に動いています！
 
-### 動作確認
+### テスト動作確認
+
+以下のコマンドでテストを動かしてみましょう。
+
+```
+npm run ut
+```
+
+```
+ PASS  __tests__/ArgsUtil.test.js
+  ✓ 引数有効判定テスト (2ms)
+  ✓ 有効引数個数テスト (1ms)
+
+Test Suites: 1 passed, 1 total
+Tests:       2 passed, 2 total
+Snapshots:   0 total
+Time:        0.962s, estimated 1s
+Ran all test suites.
+```
+
+こんな感じで表示されれば無事テストが動いています！
+
+### 実際動かしてみる
 
 以下のコマンドを実行すると「2」と表示されるかと思います。
 
---
+++ __tests__/ArgsUtil.test.js
@@ -0,0 +1,32 @@
+// import a from '../src/ArgsUtil'
+const ArgsUtil = require('../src/ArgsUtil')
+
+test('引数有効判定テスト', () => {
+  describe('argsが[1]の時の引数有効判定がfalseであること', () => {
+    const o = new ArgsUtil([1])
+    expect(o.lengthCheck()).toBe(false)
+  })
+  describe('argsが[1, 2]の時の引数有効判定がfalseであること', () => {
+    const o = new ArgsUtil([1, 2])
+    expect(o.lengthCheck()).toBe(false)
+  })
+  describe('argsが[null, null, 1]の時の引数有効判定がtrueであること', () => {
+    const o = new ArgsUtil([null, null, 1])
+    expect(o.lengthCheck()).toBe(true)
+  })
+  describe('argsが[null, null, 1, 2]の時の引数有効判定がtrueであること', () => {
+    const o = new ArgsUtil([null, null, 1, 2])
+    expect(o.lengthCheck()).toBe(true)
+  })
+})
+
+test('有効引数個数テスト', () => {
+  describe('argsが[null, null, 1]の時の有効引数個数が2であること', () => {
+    const o = new ArgsUtil([null, null, 1])
+    expect(o.length()).toBe(1)
+  })
+  describe('argsが[null, null, 1, 2]の時の有効引数個数が2であること', () => {
+    const o = new ArgsUtil([null, null, 1, 2])
+    expect(o.length()).toBe(2)
+  })
+})
