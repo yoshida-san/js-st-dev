6f4836fea62c50a3ed3a89f447163d5ec0c9c3f8
s-yoshida <yos.3104@gmail.com>
2020-02-20 18:38:34 +0900

    一部インスタンスプロパティ化してコメントを追加

--- src/ArgsUtil.js
+++ src/ArgsUtil.js
@@ -3,21 +3,23 @@ module.exports = class ArgsUtil {
    * プライベートフィールドはTC39で提案されている実験的機能です。
    * thisへの追加によって後から作成することはできません。
    * プライベートフィールドをブラウザで利用する場合はBabel等でトランスパイルすることが必要になります。
+   *
+   * 以下のコードではmodeはプライベートフィールド化し、_argsはインスタンスのプロパティとしてconstructor内で定義しています。
+   * _args = []のように定義しておくことも可能です。
    */
   #mode = ['add', 'edit', 'delete']
-  #args = []
 
   constructor() {
-    this.#args =
+    this._args =
       process.argv.length > 2 ? process.argv.slice(2, process.argv.length) : []
   }
 
-  getLength = () => this.#args.length
+  getLength = () => this._args.length
 
   getMode = () =>
     this.getLength() > 0
-      ? this.#mode.indexOf(this.#args[0]) !== -1
-        ? this.#args[0]
+      ? this.#mode.indexOf(this._args[0]) !== -1
+        ? this._args[0]
         : null
       : null
 
